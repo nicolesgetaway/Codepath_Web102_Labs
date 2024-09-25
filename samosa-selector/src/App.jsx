95fae3c785011949ae76177741614c0dc39feda4
Brooke Harris <bnharris1011@gmail.com>
2024-09-24 21:36:45 -0400

    Checkpoint 1 completed

--- samosa-selector/src/App.jsx
+++ samosa-selector/src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css';
+import samosa_img from './assets/Samosa.png'
 
 const App = () => {
 
   return (
     <div className="App">
-
+      <div className="header">
+        <h1>Samosa Selector</h1>
+        <h2>Count: </h2>
+        <img className="samosa" src={samosa_img}/>  
+      </div>
     </div>
   )
 }
