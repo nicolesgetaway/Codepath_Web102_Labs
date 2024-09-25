d4e025a0aae73a5b5080f6549828d63a67fd0d63
Brooke Harris <bnharris1011@gmail.com>
2024-09-24 22:07:50 -0400

    Checkpoint 3 completed

--- samosa-selector/src/App.jsx
+++ samosa-selector/src/App.jsx
@@ -13,6 +13,23 @@ const App = () => {
         <h1>Samosa Selector</h1>
         <h2>Count: {count} </h2>
         <img className="samosa" src={samosa_img} onClick={updateCount}/>  
+        <div className="container">
+          <div className="upgrade">
+            <h3>Double Stuffed 👯‍♀️</h3>
+            <p>2x per click</p>
+            <button>10 samosas</button>
+          </div>
+          <div className="upgrade">
+            <h3>Party Pack 🎉</h3>
+            <p>5x per click</p>
+            <button>100 samosas</button>
+          </div>
+          <div className="upgrade">
+            <h3>Full Feast 👩🏽‍🍳</h3>
+            <p>10x per click</p>
+            <button>1000 samosas</button>
+          </div>
+        </div>
       </div>
     </div>
   )
