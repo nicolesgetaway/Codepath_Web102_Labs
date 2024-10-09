5616123a48902fcc7ac4f05fae73b09a6c343d6c
Brooke Harris <bnharris1011@gmail.com>
2024-10-08 21:50:21 -0400

    Cap app setup, checkpoint 1 complete

---
+++ cap/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+import APIForm from './Components/APIform';
+
+const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
+
+function App() {
+  const [inputs, setInputs] = useState({
+    url: "",
+    width: "",
+    heigth: "",
+    no_ads: "",
+    format: "",
+  })
+
+  const submitForm = () => {
+    
+  }
+
+  return (
+    <>
+    <div className="whole-page">
+      <h1>Build Your Own Screenshot! 📸</h1>
+      
+      <APIForm
+        inputs={inputs}
+        handleChange={(e) =>
+          setInputs((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value.trim(),
+          }))
+        }
+        onSubmit={submitForm}
+      />
+      <br></br>
+
+    </div>
+    </>
+  )
+}
+
+export default App
