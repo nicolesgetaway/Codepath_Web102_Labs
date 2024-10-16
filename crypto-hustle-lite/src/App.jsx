e6dce07185b3b92b727e6b5d9fca1ea66ada31cb
Brooke Harris <bnharris1011@gmail.com>
2024-10-15 21:40:24 -0400

    Lab5: initial vite app setup and api key

---
+++ crypto-hustle-lite/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+const API_KEY = import.meta.env.VITE_APP_API_KEY;
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
