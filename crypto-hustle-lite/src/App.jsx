39f32eadcfbec364e7af5ddd1243851ea633753c
Brooke Harris <bnharris1011@gmail.com>
2024-10-16 10:27:07 -0400

    Lab5: building return statement

--- crypto-hustle-lite/src/App.jsx
+++ crypto-hustle-lite/src/App.jsx
@@ -3,12 +3,28 @@ import './App.css'
 const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [list, setList] = useState(null)
+
+  useEffect(() => {
+    const fetchAllCoinData = async () => {
+      const response = await fetch("https://min-api.cryptocompare.com/data/all/coinlist?&api_key" + API_KEY);
+      const json = await response.json();
+      setList(json);
+    }
+    fetchAllCoinData().catch(console.error);
+  }, []);
 
   return (
-    <>
-      
-    </>
+    <div className="whole-page">
+      <h1>My Crypto List</h1>
+        <ul>
+        {list && Object.entries(list.Data).map(([coin]) => 
+          list.Data[coin].PlatformType === "blockchain" ? (
+            <li key={list.Data[coin].FullName}>{list.Data[coin].FullName}</li>
+          ) : null
+        )}
+        </ul>
+    </div>
   )
 }
 
